5c1c137840beda60f3ae5de07a7413241368f6b8
Pawel Bak <pawelbak300589@gmail.com>
2021-05-08 14:01:17 +0100

    Add functionality for accessing device camera

---
+++ components/ImagePicker.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button, Image, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+import Colors from '../constants/Colors';
+
+const ImgPicker = (props) => {
+  const verifyPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.MEDIA_LIBRARY);
+    if (result.status !== 'granted') {
+      Alert.alert('Insufficient permissions!', 'You need to grant camera permissions to use this app.', [{ text: 'Okay' }]);
+      return false;
+    }
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermissions = await verifyPermissions();
+    if (!hasPermissions) {
+      return;
+    }
+    ImagePicker.launchCameraAsync();
+  };
+
+  return (
+    <View style={styles.imagePicker}>
+      <View style={styles.imagePreview}>
+        <Text>No image picket yet.</Text>
+        <Image style={styles.image} />
+      </View>
+      <Button title="Take Image" color={Colors} onPress={takeImageHandler} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imagePicker: {
+    alignItems: 'center',
+  },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default ImgPicker;
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = (props) => {
   const [titleValue, setTitleValue] = useState('');
@@ -24,6 +25,7 @@ const NewPlaceScreen = (props) => {
       <View style={styles.form}>
         <Text style={styles.label}>Title</Text>
         <TextInput style={styles.textInput} onChange={titleChangeHandler} value={titleValue} />
+        <ImagePicker />
         <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
       </View>
     </ScrollView>
