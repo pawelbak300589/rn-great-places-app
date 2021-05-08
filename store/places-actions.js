8b41b89f6b1f70ceb1de06e2f882e9a4c633f2ad
Pawel Bak <pawelbak300589@gmail.com>
2021-05-08 16:35:02 +0100

    Add fetching data from the local database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,7 +1,9 @@
 import * as FileSystem from 'expo-file-system';
 
+import { insertPlace, fetchPlaces } from '../helpers/db';
+
 export const ADD_PLACE = 'ADD_PLACE';
-import { insertPlace } from '../helpers/db';
+export const SET_PLACES = 'SET_PLACES';
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
@@ -17,6 +19,19 @@ export const addPlace = (title, image) => {
       console.log(dbResult);
 
       dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
+
+export const loadPlaces = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchPlaces();
+      console.log(dbResult);
+      dispatch({ type: SET_PLACES, places: dbResult.rows._array });
     } catch (err) {
       console.log(err);
       throw err;
