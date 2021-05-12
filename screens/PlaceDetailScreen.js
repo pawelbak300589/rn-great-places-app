f026c7523d62230b737d5b6d7fa8919cf62ea254
Pawel Bak <pawelbak300589@gmail.com>
2021-05-12 20:52:43 +0100

    Update map screen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -9,6 +9,12 @@ const PlaceDetailScreen = (props) => {
   const placeId = props.navigation.getParam('placeId');
   const selectedPlace = useSelector(state => state.places.places.find(place => place.id === placeId));
 
+  const selectedLocation = { lat: selectedPlace.lat, lng: selectedPlace.lng };
+
+  const showMapHandler = () => {
+    props.navigation.navigate('Map', { readonly: true, initialLocation: selectedLocation });
+  };
+
   return (
     <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
       <Image source={{ uri: selectedPlace.imageUri }} style={styles.image} />
@@ -16,7 +22,7 @@ const PlaceDetailScreen = (props) => {
         <View style={styles.addressContainer}>
           <Text style={styles.address}>{selectedPlace.address}</Text>
         </View>
-        <MapPreview style={styles.mapPreview} location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }} />
+        <MapPreview style={styles.mapPreview} location={selectedLocation} onPress={showMapHandler} />
       </View>
     </ScrollView>
   );
