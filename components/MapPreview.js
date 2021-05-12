3970b2a219b63568e9ede95a78c7e8c2fcc433df
Pawel Bak <pawelbak300589@gmail.com>
2021-05-12 19:02:47 +0100

    Remove old map url

--- components/MapPreview.js
+++ components/MapPreview.js
@@ -8,7 +8,6 @@ const MapPreview = (props) => {
 
   if (props.location) {
     imagePreviewUrl = `https://maps.geoapify.com/v1/staticmap?style=osm-bright-smooth&width=600&height=400&center=lonlat%3A${props.location.lng}%2C${props.location.lat}&zoom=14.3497&marker=lonlat%3A${props.location.lng}%2C${props.location.lat}%3Btype%3Aawesome%3Bcolor%3A%23bb3f73%3Bsize%3Ax-large%3Bicon%3Apaw&apiKey=${ENV().mapApiKey}`
-    // imagePreviewUrl = `https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=${ENV().mapApiKey}&lat=${props.location.lat}&lon=${props.location.lng}&vt=0&z=14`
   }
 
   return (
