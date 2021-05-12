86678259d99cf3f8bd7df0665ce61e5e2592f6df
Pawel Bak <pawelbak300589@gmail.com>
2021-05-12 20:35:25 +0100

    Display place details on the Details screen

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -19,7 +19,7 @@ const PlacesListScreen = (props) => {
     <FlatList
       data={places}
       keyExtractor={item => item.id}
-      renderItem={itemData => <PlaceItem image={itemData.item.imageUri} title={itemData.item.title} address={null} onSelect={() => {
+      renderItem={itemData => <PlaceItem image={itemData.item.imageUri} title={itemData.item.title} address={itemData.item.address} onSelect={() => {
         props.navigation.navigate('PlaceDetail', { placeTitle: itemData.item.title, placeId: itemData.item.id });
       }} />}
     />
